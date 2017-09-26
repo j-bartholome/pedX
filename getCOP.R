82eeef316d3371ce78b31914ad7fcc1f689092f0
Jessica Rutkoski <jer263@cornell.edu>
2017-09-26 10:23:36 +0800

    COP function

---
+++ getCOP.R
@@ -0,0 +1,7 @@
+#getCOP function
+getCOP<- function(gidvec, ped){
+  sp2 <- editPed(ped[,2],ped[,3], ped[,1])
+  P<- pedigree(sp2[,2],sp2[,3],sp2[,1])
+  aMx <- crossprod(relfactor(P, vec))
+  return(aMx)
+}
