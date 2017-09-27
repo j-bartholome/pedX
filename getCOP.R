9286cc8706237d7f328ec6d2f495c63f3895f869
Jessica Rutkoski <jer263@cornell.edu>
2017-09-27 10:36:55 +0800

    COP estimation function

--- getCOP.R
+++ getCOP.R
@@ -2,6 +2,8 @@
 getCOP<- function(gidvec, ped){
   sp2 <- editPed(ped[,2],ped[,3], ped[,1])
   P<- pedigree(sp2[,2],sp2[,3],sp2[,1])
-  aMx <- crossprod(relfactor(P, gidvec))
-  return(aMx)
+  tmpA<- getA(P)
+  tmp <- match(gidvec, P@label)
+  tmpA <- tmpA[tmp, tmp]
+  return(tmpA)
 }
