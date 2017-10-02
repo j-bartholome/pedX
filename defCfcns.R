cac7033f56d85cff7c0ef8c4227596063760b219
Jessica Rutkoski <jer263@cornell.edu>
2017-10-02 12:43:15 +0800

    Accepts a pedigree dataframe or sparse pedigree

---
+++ defCfcns.R
@@ -0,0 +1,22 @@
+defCfcns<-function(){
+  incl<- 'using Eigen::LLT;
+  using Eigen::Lower;
+  using Eigen::Map;
+  using Eigen::MatrixXd;
+  using Eigen::MatrixXi;
+  using Eigen::Upper;
+  using Eigen::VectorXd;
+  typedef Map<MatrixXd> MapMatd;
+  typedef Map<MatrixXi> MapMati;
+  typedef Map<VectorXd> MapVecd;
+  '
+  sparseProdCpp<- 'using Eigen::MappedSparseMatrix;
+  using Eigen::SparseMatrix;
+  const MappedSparseMatrix<double> A(as<MappedSparseMatrix<double> >(AA));
+  const SparseMatrix<double> Acr(A.adjoint() * A);
+  return List::create(Named("Acr") = Acr.triangularView<Lower>());'
+  sparse1 <- cxxfunction(signature(AA = "dgCMatrix"),
+                         sparseProdCpp, "RcppEigen", incl)
+  return(environment())
+}
+
---
+++ getCOPxl.R
@@ -0,0 +1,23 @@
+#################
+# getCOPxl
+#' Function for getting a large sparce relationship (COP) matrix
+#' from a sparse pedigree matrix. This uses some matrix multiplication
+#' in C++ using Eigen, its still not faster than R
+#' 
+#'  Parameters:
+#'  @P Sparse pedigree matrix (class pedigree from pedigreemm)
+#'  
+#'  @return A sparse relationship matrix
+#'  
+################
+getCOPxl<- function(P, Cfun){
+  assign('rect', Diagonal(x = sqrt(Dmat(P))) %*% 
+           solve(t(as(P, "sparseMatrix")), 
+            as(factor(P@label, levels = P@label), 
+            "sparseMatrix")),envir=Cfun)
+  Amat <- with(Cfun, sparse1(rect)[[1]])
+  #dimnames(Amat)[[1]] <- dimnames(Amat)[[2]] <- P@label
+  return(Amat)
+}
+
+
