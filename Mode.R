eeb68a7d191a2562965453b0afc53c9c1da52eca
Jessica Rutkoski <jer263@cornell.edu>
2017-10-06 13:17:22 +0800

    redundant

--- Mode.R
+++
@@ -1,6 +0,0 @@
-##################################
-#Function for finding the mode
-Mode <- function(x) {
-  ux <- unique(x)
-  ux[which.max(tabulate(match(x, ux)))]
-}
--- gidMax.R
+++
@@ -1,9 +0,0 @@
-gidMax<- function(gidvec, dbenv){
-  nhits<- c()  
-  for(i in 1:length(gidvec)){
-    assign('gid', gidvec[i], envir=dbenv)
-    nhit<- nrow(with(dbe, dbGetQuery(con, sprintf("SELECT * FROM germplsm WHERE gpid2=%d",gid))))
-    nhits<- append(nhits, nhit)
-  }
-  return(gidvec[which.max(nhits)])
-}
