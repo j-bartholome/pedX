84384f9ba77881007feddb66a988aa1c6e447559
Jessica Rutkoski <jer263@cornell.edu>
2017-10-06 15:10:32 +0800

    bug fix

--- R/getCOP.R
+++ R/getCOP.R
@@ -1,12 +1,16 @@
 #' getCOP Function for getting a sparse relationship (COP) matrix
 #'
 #' @import pedigreemm
+#' @import Matrix
 #' @param Ped Sparse pedigree matrix (class pedigree from pedigreemm) OR a data fame
 #' @param gidvec a vector of gids of interest
 #' @return A sparse relationship matrix
 #' @export
 #'
-getCOP<- function(gidvec, ped){
+getCOP<- function(gidvec=NULL, ped){
+  if(is.null(gidvec)){
+    gidvec<- ped[,'label']
+  }
   if(class(ped)[1]=="data.frame"){
     sp2 <- editPed(ped[,'sire'],ped[,'dam'], ped[,'label'])
     P<- pedigree(sp2[,2],sp2[,3],sp2[,1])
@@ -14,9 +18,8 @@ getCOP<- function(gidvec, ped){
   if(class(ped)[1]=='pedigree'){
     P<- ped
   }
-  rect <- Diagonal(x = sqrt(Dmat(P))) %*% solve(t(as(P,
-    "sparseMatrix")), as(factor(P@label, levels = P@label), "sparseMatrix"))
-  tmpA <- crossprod(rect)
+  rect <- Diagonal(x = sqrt(Dmat(P))) %*% Matrix::solve(Matrix::t(as(P,"sparseMatrix")), as(factor(P@label, levels = P@label), "sparseMatrix"))
+  tmpA <- Matrix::crossprod(rect)
   tmp <- match(gidvec, P@label)
   tmpA <- tmpA[tmp, tmp]
   return(tmpA)
