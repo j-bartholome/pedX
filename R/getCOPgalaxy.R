e395d50dde783e69e96df37bf1e3f4cca3754bc6
Jessica Rutkoski <jer263@cornell.edu>
2018-02-06 15:04:41 +0800

    fix galaxy integration
    fix galaxy integration

--- R/getCOPgalaxy.R
+++ R/getCOPgalaxy.R
@@ -1,13 +1,14 @@
 #' getCOPgalaxy wrapper function for getting a sparse relationship (COP) matrix
 #'
+#' @import RGalaxy
 #' @param pathPed character path to pedigree
 #' @param pathIds character path to ids of interest
 #' @param ordered boolean if pedigree is ordered or not
 #' @export
 #' 
 getCOPgalaxy<- function(pathPed, pathIds, ordered){
-  pd<- read.csv(pathPed, row.names=1)
-  ids<- read.csv(pathIds)[,1]
+  pd<- read.csv(GalaxyCharacterParam(pathPed), row.names=1)
+  ids<- read.csv(GalaxyCharacterParam((pathIds))[,1]
   cp<- getCOP(ids, pd, ordered=ordered)
   save.image(cp, file='COPsparse.RData')
 }
