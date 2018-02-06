adfa07d0620f0142f9d3ca7904ba7dc92fcef9b6
Jessica Rutkoski <jer263@cornell.edu>
2018-02-06 19:38:36 +0800

    change input type
    change input type

--- R/getCOPgalaxy.R
+++ R/getCOPgalaxy.R
@@ -1,13 +1,13 @@
 #' getCOPgalaxy wrapper function for getting a sparse relationship (COP) matrix
 #'
 #' @import RGalaxy
-#' @param pathPed character path to pedigree
-#' @param pathIds character path to ids of interest
+#' @param pathPed galaxy input file for pedigree
+#' @param pathIds galaxy input file for ids of interest
 #' @param ordered boolean if pedigree is ordered or not
 #' @export
-#' 
-getCOPgalaxy<- function(pathPed=GalaxyCharacterParam(required=TRUE), 
-                        pathIds= GalaxyCharacterParam(required=TRUE), 
+#'
+getCOPgalaxy<- function(pathPed=GalaxyInputFile(required=TRUE, formatFilter=character(0)),
+                        pathIds= GalaxyInputFile(required=TRUE, formatFilter=character(0)),
                         ordered= GalaxyLogicalParam(required=TRUE),
                         outputfile= GalaxyOutput('COPsparse', 'RData')){
   pd<- read.csv(pathPed, row.names=1)
