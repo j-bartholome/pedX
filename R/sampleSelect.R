bd574e1d0375b77fc9f9f043d50bd12316bb4f13
Jessica Rutkoski <jer263@cornell.edu>
2018-02-18 10:27:34 +0800

    remove galaxy fcn
    remove galaxy fcn

--- R/sampleSelect.R
+++ R/sampleSelect.R
@@ -8,7 +8,7 @@
 #' @param ntry is the number of iterations of the exchange algorithm
 #' @param nrep is the number of repitions of the exchange algorithm
 #'
-sampleSelect2<- function(cp, cand, target, nchoose, herit=0.5, ntry=300, nrep=30, contrast='Pop'){
+sampleSelect<- function(cp, cand, target, nchoose, herit=0.5, ntry=300, nrep=30, contrast='Pop'){
   
 
   
