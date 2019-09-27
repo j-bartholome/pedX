7449e8c4f9ed9014c6e97c667d684dcaa5fa592b
j-bartholome <jerome.bartholome@cirad.fr>
2019-09-27 13:01:08 +0800

    Add host as parameter in cnGMS in case the host is changed.

--- R/cnGMS.R
+++ R/cnGMS.R
@@ -1,22 +1,29 @@
 #' Setting the connection
 #'
 #' @import RPostgreSQL
-#' @param usr character username
-#' @param pws character passward
-#' @return the object which is the function environment
+#'
+#' @param usr a character string for the "username"
+#' @param pws a character string for the "password"
+#' @param host a character string for the ip address of the host
+#' @return the function environment
 #' @export
 #'
-cnGMS<- function(usr, psw){
-  yourusername= usr
-  yourpassword= psw
+cnGMS<- function(usr, psw, host){
+  yourusername <- usr
+  yourpassword <- psw
 
   #Driver is PostgreSQL not RPostgreSQL
   drv <- dbDriver("PostgreSQL")
 
   #Replace user and pass. Leave dbname, hostname, and port as is.
-  con <- dbConnect(drv, dbname = "central_iris",
-                 host = "172.29.4.52", port = 5432,
-                 user = yourusername, password = yourpassword)
+  con <- dbConnect(
+    drv,
+    dbname = "central_iris",
+    host = host,
+    port = 5432,
+    user = yourusername,
+    password = yourpassword
+  )
   df_postgres<- dbGetQuery(con, "SET search_path = gms")
 return(environment())
 }
